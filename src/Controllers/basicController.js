05355a261b2f5caf466ef66a9b72dca663c2320d
nitch193 <niteshchaurasiya193@gmail.com>
2021-06-21 19:39:06 +0530

    user validation and Jwt authentication added

--- src/Controllers/basicController.js
+++ src/Controllers/basicController.js
@@ -2,7 +2,7 @@ const basicController = {};
 
 basicController.get = (req, res) => {
   res.json({
-    message: "Welocpmce",
+    message: "Welocom",
   });
 };
 
--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -5,40 +5,44 @@ import { loginValidation, signupValidation } from "../validation";
 const userController = {};
 
 userController.signup = async (req, res) => {
-  const { username, password, email } = req.body;
-
   const { error } = signupValidation(req.body);
   if (error) return res.status(400).json({ message: error.details });
-
-  const emailexist = await db.User.findOne({ email: email });
-  const usernameExist = await db.User.findOne({ username: username });
-  if (emailexist)
-    return res.status(400).res.send({ message: "Email alreasy exists" });
-  if (usernameExist)
-    return res
-      .status(400)
-      .res.json({ message: " This Username alreasy exists" });
-
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const user = new db.User({
-    username: username,
-    password: hashedPassword,
-    email: email,
-  });
-
-  user
-    .save()
-    .then((newUser) => {
-      res.status(200).json({
-        success: true,
-        data: newUser,
-      });
+  db.User.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }],
+  })
+    .then(async (user) => {
+      if (user) {
+        let errors = {};
+        if (user.username === req.body.username)
+          errors.username = "This username is taken";
+        if (user.email === req.body.email)
+          errors.email = "Email already in use";
+        return res.status(400).json(errors);
+      } else {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const user = new db.User({
+          username: req.body.username,
+          password: hashedPassword,
+          email: req.body.email,
+        });
+        user
+          .save()
+          .then((newUser) => {
+            res.status(200).json({
+              success: true,
+              data: newUser,
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message: err,
+            });
+          });
+      }
     })
     .catch((err) => {
-      res.status(500).json({
-        message: err,
-      });
+      return res.status(500).json({ messsage: err });
     });
 };
 
@@ -46,20 +50,22 @@ userController.login = async (req, res) => {
   const { email, password } = req.body;
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).json({ message: error.details });
-  const user = await db.User.findOne({ email: email });
+  const user = await db.User.findOne({ email: email }).catch((err) => {
+    return res.status(500).json({ message: err });
+  });
   if (!user)
-    return res.status(404).res.json({
+    return res.status(404).json({
       message: "User not found",
     });
 
   const validPass = await bcrypt.compare(password, user.password);
-
   if (!validPass)
-    return res.status(400).res.json({
+    return res.status(400).json({
       message: "Password is incorrect",
     });
 
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token);
   return res.status(200).json({
     success: true,
     accessToken: token,
